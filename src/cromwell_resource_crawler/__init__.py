3c9eafeeed1c56847a71e8fd42904c936d33d78b
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-06-02 15:20:16 +0200

    work on resource crawler skeleton

--- src/cromwell_resource_crawler/__init__.py
+++ src/cromwell_resource_crawler/__init__.py
@@ -17,3 +17,58 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+from pathlib import Path
+from typing import Generator, List, Dict, Union, Tuple, Optional
+
+
+def recursive_iterdir(path: Path) -> Generator[Path, None, None]:
+    for sub_path in path.iterdir():
+        if sub_path.is_dir():
+            for sub_sub_path in recursive_iterdir(sub_path):
+                yield sub_sub_path
+        else:
+            yield sub_path
+
+
+class Job():
+    def __init__(self, path: Path, id: Tuple[str]):
+        self.path: Path = path
+        self.id = id
+        executions = path / "executions"
+        self.stdout_submit: Path = executions / "stdout.submit"
+        self.inputs: List[Path] = list(recursive_iterdir(path / "inputs"))
+        self.resources: Dict[str, Union[float, int]] = self.get_resources()
+
+    def get_resources(self):
+        return {}
+
+
+def crawl_workflow_folder(workflow_folder: Path, jobclass: Job = Job,
+                          id: Optional[List[str]] = None
+                          ) -> Generator[Job, None, None]:
+    base_id = id or []
+    for uuid in workflow_folder.iterdir():
+        this_id = base_id + [uuid.name]
+        for call_folder in uuid.iterdir():
+            for job in crawl_call_folder(call_folder, jobclass, this_id):
+                yield job
+
+
+def crawl_call_folder(call_folder: Path, jobclass: Job = Job,
+                      id: Optional[List[str]] = None
+                      ) -> Generator[Job, None, None]:
+    base_id = id or []
+    this_id = base_id + [call_folder.name]
+    if Path(call_folder, "cacheCopy").exists():
+        return
+    if Path(call_folder, "execution").exists():
+        yield jobclass(call_folder, tuple(this_id))
+    elif Path(call_folder, "shard-0").exists():
+        for shard in call_folder.iterdir():
+            for job in crawl_call_folder(shard, jobclass, this_id):
+                yield job
+    else:
+        for workflow_folder in call_folder.iterdir():
+            for job in crawl_workflow_folder(workflow_folder, jobclass, this_id):  # noqa: E502
+                yield job
